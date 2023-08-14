9271a2a3859e5b9befc30f6af67a8e8243bd94c6
Benson gathu <bensongathu23@gmail.com>
2023-08-14 11:16:51 +0300

    Section two Done:

--- lib/cards.ex
+++ lib/cards.ex
@@ -1,25 +1,68 @@
 defmodule Cards do
-   
   def create_deck do
-    values = ["Ace","Two","Three","Four","Five","Six","Seven","Eight","Nine","Ten","Jack","Queen","King"]
-    suits =["Spades","Clubs","Hearts","Diamonds"]
+    values = [
+      "Ace",
+      "Two",
+      "Three",
+      "Four",
+      "Five",
+      "Six",
+      "Seven",
+      "Eight",
+      "Nine",
+      "Ten",
+      "Jack",
+      "Queen",
+      "King"
+    ]
+
+    suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
 
     for value <- values, suit <- suits do
-        "#{value} of #{suit}"
-      
+      "#{value} of #{suit}"
     end
   end
 
-  def shuffle(deck) do 
-   Enum.shuffle(deck)
+  def shuffle(deck) do
+    Enum.shuffle(deck)
   end
 
-  def contains?(deck, hand) do 
-    Enum.member?(deck,hand)
+  def contains?(deck, hand) do
+    Enum.member?(deck, hand)
   end
 
-  def deal(deck,hand_size) do 
-    Enum.split(deck,hand_size)
+  def deal(deck, hand_size) do
+    Enum.split(deck, hand_size)
   end
 
+  def save(deck, filename) do
+    binary = :erlang.term_to_binary(deck)
+    File.write(filename, binary)
+  end
+
+  def load(filename) do
+    {status, binary} = File.read(filename)
+
+    case status do
+      :ok -> :erlang.binary_to_term(binary)
+      :error -> "That file doesn't exist"
+    end
+  end
+
+  # def load(filename) do
+  #   case File.read(filename) do
+  #     {:ok, binary} -> :erlang.binary_to_term(binary)
+  #     {:error, reason }-> "That file does not exist"
+  #   end
+  # end
+  def create_hand(hand_size) do
+    # deck = Cards.create_deck
+    # deck = Cards.shuffle(deck)
+    # hand =  Cards.deal(deck,hand_size)
+
+    # the above code in a pipe operator
+    Cards.create_deck()
+    |> Cards.shuffle()
+    |> Cards.deal(hand_size)
+  end
 end
