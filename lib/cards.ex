39b6ea81f98bd814e1024892d95691efa0993355
Benson gathu <bensongathu23@gmail.com>
2023-08-14 14:22:30 +0300

    tests: doctests and case tests added

--- lib/cards.ex
+++ lib/cards.ex
@@ -1,4 +1,12 @@
 defmodule Cards do
+  @moduledoc"""
+    Provides methods for creating and handlind a deckmof cards
+    """
+
+
+  @doc """
+  Returns a list of strings representing a deck of playing cards
+  """
   def create_deck do
     values = [
       "Ace",
@@ -26,11 +34,35 @@ defmodule Cards do
   def shuffle(deck) do
     Enum.shuffle(deck)
   end
+  @doc """
+    Determeines whether a deck contains a given card
 
+  ## Examples
+
+
+      iex> deck = Cards.create_deck
+      iex> Cards.contains?(deck,"Ace of Spades")
+      true
+
+
+
+  """
   def contains?(deck, hand) do
     Enum.member?(deck, hand)
   end
 
+  @doc """
+    Divides a deck into a hand and the remainder of the deck.
+    The `hand_size` argument indicated how many cards should be in the hand
+
+  ## Examples
+
+        iex> deck = Cards.create_deck
+        iex> {hand, deck} = Cards.deal(deck,1)
+        iex> hand
+        ["Ace of Spades"]
+
+  """
   def deal(deck, hand_size) do
     Enum.split(deck, hand_size)
   end
--- test/cards_test.exs
+++ test/cards_test.exs
@@ -1,8 +1,18 @@
 defmodule CardsTest do
   use ExUnit.Case
+
+  # doctests (tests examples in the codes documentation)
   doctest Cards
 
-  test "greets the world" do
-    assert Cards.hello() == :world
+  # Case tests
+  test "create_deck makes 20 cards" do
+    deck_length = length(Cards.create_deck())
+    assert deck_length == 52
+  end
+
+  test "shuffle_working" do
+
+    deck = Cards.create_deck
+    assert deck != Cards.shuffle(deck)
   end
 end
